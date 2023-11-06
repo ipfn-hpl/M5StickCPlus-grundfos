010cfa2cff0c23d9c4de96d0a9cc1acbd7dedb33
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-11-06 12:07:51 +0000

    Updated code
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 * Description: Adjust the speed of FAN Unit through PWM.
 * https://docs.m5stack.com/en/core/m5stickc_plus
 * https://github.com/m5stack/m5-docs/blob/master/docs/en/api/button.md
+* LCD screen 	1.14 inch, 135*240 Colorful TFT LCD, ST7789v2
 */
 
 #include <M5StickCPlus.h>
@@ -32,18 +33,37 @@ int resolution      = 10;
 unsigned long lastMsg = 0;
 unsigned long lastLed = 0;
 unsigned int pwmLed = 0;
-unsigned int pwmMotor = 800;
+unsigned int pwmMotor = 110;
 
 unsigned long lastRising = 0;
 long pulseWidth = 0;
 long pulsePeriod = 0;
+const long max_period = 13333;
 
-unsigned long countRising = 0;
-unsigned long countFalling = 0;
-bool speed = false;
+/* PWM Levels
+Descrição
+Subida: 1 W / % PWM
+0-70 % <9333 Potência [W]
+25% 3333 Potência 25 W (Max)
+2: 95% 12666 Reserva (paragem)
+3: 90% 12000 Paragem de alarme: avaria, circulador bloqueado
+4: 85 % 11333 Paragem de alarme: avaria elétrica
+5: 80% 10666 Aviso
+6: 70 % 9333 Saturação
+*/
+
+long countRising = 0;
+long countFalling = 0;
+//bool speed = false;
+void IRAM_ATTR pulse_width_ris_isr(void);
 void IRAM_ATTR pulse_width_fall_isr(void) {
+    long tmp;
     detachInterrupt(pulse_width_pin);
-    pulseWidth =  micros() - lastRising;
+    tmp =  micros() - lastRising;
+   // if ((tmp > 100) && (tmp < 3000))
+        pulseWidth =  tmp;
+    if(countRising > 0)
+        attachInterrupt(pulse_width_pin, pulse_width_ris_isr, RISING);
     countFalling++;
 }
 
@@ -51,10 +71,10 @@ void IRAM_ATTR pulse_width_ris_isr(void) {
     pulsePeriod =  micros() - lastRising;
     lastRising = micros();
     detachInterrupt(pulse_width_pin);
-    pulseWidth = 0;
-    countRising++;
-
-    attachInterrupt(pulse_width_pin, pulse_width_fall_isr, FALLING);
+    //pulseWidth = 0;
+    countRising--;
+    if(countRising > 0)
+        attachInterrupt(pulse_width_pin, pulse_width_fall_isr, FALLING);
 }
 
 void setup() {
@@ -63,8 +83,13 @@ void setup() {
     Serial.begin(115200);
     M5.Lcd.setRotation(3);
     //setCursor(int16_t x0, int16_t y0, uint8_t font)
+    M5.Lcd.fillScreen(BLACK);
     M5.Lcd.setCursor(25, 0, 2);
     M5.Lcd.println("Grundfos ALPHA1L");
+    M5.Lcd.setCursor(0, 20);
+    M5.Lcd.print("PWM: ");
+    M5.Lcd.setCursor(0, 40);
+    M5.Lcd.print("Pulse: ");
     ledcSetup(fanChannel, freq, resolution);
     ledcAttachPin(motor_pin, fanChannel);
     ledcSetup(ledChannel, freq, resolution);
@@ -91,27 +116,28 @@ void setup() {
 }
 
 void loop() {
+    float motorPower;
     M5.update();
     if (M5.BtnA.wasReleased()) {  // If the button A is pressed. 
-        //M5.Lcd.print('A');
         pwmMotor = 112;
         Serial.print("A, PWM:");
         Serial.println(pwmMotor);
-        M5.Lcd.setCursor(10, 20);
-        M5.Lcd.print("PWM: ");
+        M5.Lcd.setCursor(80, 20);
+        //M5.Lcd.print("PWM: ");
         M5.Lcd.print(pwmMotor);
+        M5.Lcd.setCursor(120, 20);
+        M5.Lcd.print('A');
     } else if (M5.BtnB.wasReleased()) {  // If the button B is pressed. 
-        //M5.Lcd.print('B');
         pwmMotor = 800;
         Serial.print("B, PWM:");
         Serial.println(pwmMotor);
-        M5.Lcd.setCursor(10, 20);
-        M5.Lcd.print("PWM: ");
+        M5.Lcd.setCursor(80, 20);
+        //M5.Lcd.print("PWM: ");
         M5.Lcd.print(pwmMotor);
+        M5.Lcd.setCursor(120, 20);
+        M5.Lcd.print('B');
     }
 
-    float pwmFloat;
-    // put your main code here, to run repeatedly:
     //ledcWrite(ledChannel, 512);
     //ledcWrite(ledChannel, 10);
     unsigned long now =  millis();  // Obtain the host startup duration.
@@ -129,6 +155,7 @@ void loop() {
     if (now - lastMsg > 1000) {
         lastMsg = now;
         detachInterrupt(pulse_width_pin);
+        countRising = 2;
         attachInterrupt(pulse_width_pin, pulse_width_ris_isr, RISING);
         Serial.print("Time:");
         Serial.print(now/1000);
@@ -143,32 +170,44 @@ void loop() {
         Serial.print(" F:");
         Serial.print(countFalling);
         
-        pwmFloat = pulseWidth;
-        pwmFloat = pwmFloat / pulsePeriod;
+        motorPower = pulseWidth;
+        motorPower = motorPower / 3333.0 * 25.0; // in W
+        //if(pulsePeriod > 0.01)
+        //pwmFloat = pwmFloat / pulsePeriod;
         Serial.print(" %:");
-        Serial.println(pwmFloat);
+        Serial.println(motorPower);
 
         sensors.requestTemperatures();
         float temperatureC = sensors.getTempCByIndex(0);
         Serial.print(temperatureC);
         Serial.println("ºC");
-
-        M5.Lcd.setCursor(10, 20);
-        M5.Lcd.print("PWM: ");
+        M5.Lcd.setCursor(80, 20);
+        M5.Lcd.print("      ");
+        M5.Lcd.setCursor(80, 20);
         M5.Lcd.print(pwmMotor);
-        M5.Lcd.setCursor(10, 40);
-        M5.Lcd.print("Pulse: ");
-        M5.Lcd.println(pulseWidth);
-        M5.Lcd.setCursor(10, 60);
-        M5.Lcd.print("pF: ");
-        M5.Lcd.println(pwmFloat);
+        M5.Lcd.setCursor(80, 40);
+        M5.Lcd.print(pulseWidth);
+        if((pulseWidth > 10) && (pulseWidth < 3333)) {
+            M5.Lcd.setCursor(10, 60);
+            M5.Lcd.print("      ");
+            M5.Lcd.setCursor(10, 60);
+            M5.Lcd.print(motorPower,2);
+            M5.Lcd.print(" W");
+        }
+        else {
+            M5.Lcd.setCursor(120, 60);
+            M5.Lcd.print("NV");
+        }
+        M5.Lcd.setCursor(10, 80);
+        M5.Lcd.print("Period: ");
+        M5.Lcd.print(pulsePeriod);
         /*
         if (speed)
             ledcWrite(fanChannel, 600);
         else
             ledcWrite(fanChannel, 300);
             */
-        speed = not speed;
+        //speed = not speed;
 
     }
 }
